a137bb43def85fd60f2a6009a0b76f8afa18fc04
AyeshadevX <aishaasghar436@gmail.com>
2025-08-15 01:01:30 +0500

    Replace comparison logic with last digit check and remove unused variables

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-let a=5;
-let b=18;
-let c=13;
+let num1=32;
+let num2=47852;
 
-if(a>c){
-    console.log(a, "is larger");
-}else {
-    console.log(c,"is larger");
-}else {
-    if(b>c){
-        console.log(b,"is larger");
-    }else{
-        console.log(c,"is larger");
-    }
+if((num1%10)==(num2%10)){
+    console.log("numbers have same last digit which is",num1%10);
+}else{
+    console.log("number don't have same last digit");
 }
