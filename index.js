b3a07979b242b5320a6ff29c4927778c8ccb44d7
AyeshadevX <aishaasghar436@gmail.com>
2025-08-17 21:30:47 +0500

    Replace number guessing game logic with simple dice roll

--- index.js
+++ index.js
@@ -1,23 +1,2 @@
-const max = prompt("Enter the max number");
-
-const random = Math.floor(Math.random() * max) + 1;
-
-let guess = prompt("Guess the number");
-
-while (true){
-    if(guess == "quit"){
-        console.log("user quit);
-            break;
-        }
-        if (guess == random){
-            console.log("you are right");
-            break;
-        }else if {
-             guess =prompt("your guess was wrong try again");
-        }else{
-            guess = prompt("your guess was too large please try again");
-        }
-        
-        
-     
-}
+let dice = Math.floor(Math.random()* 6)+1;
+console.log(dice);
