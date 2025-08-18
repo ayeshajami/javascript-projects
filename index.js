f3473385b2e0678429289835d8c4a737f71319fa
AyeshadevX <aishaasghar436@gmail.com>
2025-08-18 21:39:54 +0500

    Refactor code to remove unused functions and retain printTable functionality

--- index.js
+++ index.js
@@ -1,35 +1,6 @@
-
-function rollDice(){
-let rand=Math.floor(Math.random()*6)+1;
-console.log("rand");
+function printTable(n){
+for(let i=n; i<=n*10; i+=10){
+    console.log(i);
 }
-rollDice();
 
-function printName(name){
-    console.log(name);
-}
- printName("Aisha");
-
- function printInfo(name,age){
-    console.log(name);
-    console.log(age);
-}
- printInfo("Aisha");
-  printInfo(21);
-
-
- function sum(a,b){
-    console.log(a+b);
- }
-
- sum(1,3);
-sum(4,5);
-
-
-function avg(a,b,c){ 
-
-    let avg=(a+b+c)/3;
-    console.log(avg);
-
-}
-avg(5,6,7);
+printTable(5);
