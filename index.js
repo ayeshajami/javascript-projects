06b34511ecbfdfa29bde942235e417e904d99dad
AyeshadevX <aishaasghar436@gmail.com>
2025-08-19 19:08:00 +0500

    Refactor index.js to improve code structure and maintainability

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const sum =(a,b) =>{
     console.log(a+b);
 }
 const mul=(a,b)=>(a*b);
+
+console.log("Hi there!");
+
+setTimeout(()=>{
+    console.log("Aisha");
+},4000);
+console.log("Welcome");
+
