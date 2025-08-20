4cf3d94d75a8b5f96ff8d64241fbacf2a1a78377
AyeshadevX <aishaasghar436@gmail.com>
2025-08-20 21:17:56 +0500

    Refactor index.html and index.js to remove unused elements and streamline event handling

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
-let btn=document.querySelector("button");
+let p=document.querySelector("p");
 
-btn.addEventListener("click",function(){
-  let h3=document.querySelector("h3");
-  let randomColor=getRandomColor();
-  h3.innerText = randomColor;
+p.addEventListener("click",function(){
+  console.log("parah was clicked");
+});
 
-  let div=document.querySelector("div");
-  div.style.backgroundColor=randomColor;
+let box=document.querySelector(".box");
+box.addEventListener("mouseenter",function(){
+  console.log("mouse inside box");
 
-
-  console.log("color updated");
-})
-
-function getRandomColor(){
-  let red=Math.floor(Math.random()*225);
-  let green=Math.floor(Math.random()*225);
-  let blue=Math.floor(Math.random()*225);
-  
-  let color = `rgb(${red}, ${green}, ${blue})`;
-
-  return color;
-}
+});
