39f7befd392aa88b3eeca7e699d5f0ab2c21b8e7
AyeshadevX <aishaasghar436@gmail.com>
2025-08-20 19:53:09 +0500

    Fix typo in button click event assignment and refactor event handler

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 let btn=document.querySelector("button");
 console.dir(btn);
-btn.onclick = function(){
-  console.log("Button clicked!");
-  alert("button clicked!");
+// btn.onclick = function(){
+//   console.log("Button clicked!");
+//   alert("button clicked!");
+// }
+
+function sayHellow(){
+  alert("Hellow!");
 }
+
+btn.onlick=sayHellow;
