3a00a9896f382927b03fb303196684c0782d0ed3
AyeshadevX <aishaasghar436@gmail.com>
2025-08-21 21:31:15 +0500

    Refactor form structure and remove unused keyup event logging

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
-let inp = document.querySelector("input");
+let form=document.querySelector("form");
 
-// inp.addEventListener("keydown", function () {
-//   console.log("key was pressed");
-// });
-
-inp.addEventListener("keyup", function () {
-  console.log("key=",event.key);
-
-  if(event.key =="Arrowup"){
-  console.log("character moves forward");
-  }
-  else if(event.key =="ArrowDown"){
-console.log("character moves backward");
-  }
-  
-  else if(event.key =="ArrowLeft"){
-    console.log("character moves left");
-  }
- 
-  
-  else if(event.key =="ArrowRight"){
-  console.log("character moves right");
-}
+form.addEventListener("submit",function(event){
+  event.preventDefault();
+  alert("form submit");
 });
-
