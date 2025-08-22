b6b3fb3e0ff4a6b48e1780e28481020c6b1948a1
AyeshadevX <aishaasghar436@gmail.com>
2025-08-22 17:07:53 +0500

    Refactor game structure by removing todo app components and adding game logic

---
+++ app.js
@@ -0,0 +1,15 @@
+let gameSeq=[];
+let userSeq=[];
+
+let started=false;
+let level=0;
+
+document.addEventListener("keypress",function(){
+if(started== false){
+    console.log("game started");
+    started=true;
+
+    levelup();
+}
+});
+
