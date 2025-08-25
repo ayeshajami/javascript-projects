0cf913048bffc76ee6aa2253a44a1d4c6411b90b
AyeshadevX <aishaasghar436@gmail.com>
2025-08-25 15:35:16 +0500

    Comment out unused color change functionality for future reference

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-h1=document.querySelector("h1");
+// h1=document.querySelector("h1");
   
 
-function changeColor(color,delay,nextColorChange){
-    setTimeout(()=>{
- h1.style.color=color;
- if(nextColorChange)nextColorChange();
-    },delay);
+// // function changeColor(color,delay,nextColorChange){
+// //     setTimeout(()=>{
+// //  h1.style.color=color;
+// //  if(nextColorChange)nextColorChange();
+// //     },delay);
    
+// // }
+// // changeColor("red",1000,()=>{
+// //     changeColor("orange",1000,()=>{
+// //      changeColor("green",1000);
+// //     });
+// // });
+
+function savetoDb(data,success,failer){
+    let internetSpeed=Math.floor(Math.random()*10)+1;
+    if(internetSpeed>4){
+        success();
+    }else{
+        failer();
+    }
 }
-changeColor("red",1000,()=>{
-    changeColor("orange",1000,()=>{
-     changeColor("green",1000);
-    });
-});
-
-
+savetoDb("apna college",()=>{
+    console.log("your data was saved");
+    savetoDb("aisha",()=>{
+        console.log("save");
+    })
+})
