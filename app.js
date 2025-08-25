d6408cbf86094e7b50f37cb33161dc864650adce
AyeshadevX <aishaasghar436@gmail.com>
2025-08-25 15:42:12 +0500

    Remove commented-out code and unused functions for improved code clarity

--- app.js
+++ app.js
@@ -1,34 +1,3 @@
-// h1=document.querySelector("h1");
-  
-
-// // function changeColor(color,delay,nextColorChange){
-// //     setTimeout(()=>{
-// //  h1.style.color=color;
-// //  if(nextColorChange)nextColorChange();
-// //     },delay);
-   
-// // }
-// // changeColor("red",1000,()=>{
-// //     changeColor("orange",1000,()=>{
-// // //      changeColor("green",1000);
-// // //     });
-// // // });
-
-// function savetoDb(data,success,failer){
-//     let internetSpeed=Math.floor(Math.random()*10)+1;
-//     if(internetSpeed>4){
-//         success();
-//     }else{
-//         failer();
-//     }
-// }
-// savetoDb("apna college",()=>{
-//     console.log("your data was saved");
-//     savetoDb("aisha",()=>{
-//         console.log("save");
-//     })
-// })
-
 
 function savetoDb(data){
     
