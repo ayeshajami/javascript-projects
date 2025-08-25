fc9be4a685af17b61cca6dd38d650f672e1a0f12
AyeshadevX <aishaasghar436@gmail.com>
2025-08-25 14:39:48 +0500

    Refactor greeting messages with delays for improved user experience

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-setTimeout(function() {
-    console.log)("Hello,World!");
- },200);
- console.log("Hello...");
+setTimeout(()=>{
+    console.log("Hello World");
+},2000);
+setTimeout(()=>{
+    console.log("Hello Aisha ");
+},3000);  
+console.log("Hi");
