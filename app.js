802d46a07a7044184a93827e481ecb3923f203d2
AyeshadevX <aishaasghar436@gmail.com>
2025-08-25 14:11:20 +0500

    Remove game logic and HTML elements related to the Simon Says game

--- app.js
+++ app.js
@@ -1,56 +1,10 @@
-let gameSeq=[];
-let userSeq=[];
-
-let btns=["yellow","red","green","purple"];
-let started=false;
-let level=0;
-
-let h2 = document.querySelector("h2");
-
-document.addEventListener("keypress",function(){
-if(started == false){
-    console.log("game started");
-    started=true;
-
-    levelup();
+function one (){
+    return 1;
 }
-});
-function gameFlash(btn){
-    btn.classList.add("flash");
-    setTimeout(function(){
-        btn.classList.remove("flash");
-    },250);
+function two(){
+    return one()+one();
 }
-function userFlash(btn){
-    btn.classList.add("userflash");
-    setTimeout(function(){
-        btn.classList.remove("userflash");
-    },250);
-}
-function levelup() {
-    level++;
-    h2.innerText = `Level ${level}`;
-
-    let randIdx = Math.floor(Math.random() * 4);
-    let randColor = btns[randIdx];
-    let randBtn = document.querySelector(`.${randColor}`);
-    // console.log(randIdx);
-    // console.log(randColor);
-    // console.log(randBtn);
-    gameSeq.push(randColor);
-    console.log(gameSeq);
-    gameFlash(randBtn);
-
-}
-
-function btnPress(){
-    console.log(this);
-    let btn = this;
-    userFlash(btn);
-}
-
-let allBtns=document.querySelectorAll(".btn");
-for(btn of allBtns){
-    btn.addEventListener("click",btnPress);
-
+function three(){
+    let ans= two()+one();
+    console.log(ans);
 }
