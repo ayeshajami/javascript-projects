20f0fbb51f65c5111b3b08b740894e84206dc62e
AyeshadevX <aishaasghar436@gmail.com>
2025-08-27 17:40:21 +0500

    Enhance changeColor function with rejection handling and update demo to await all calls

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ h1=document.querySelector("h1");
 function changeColor(color,delay){
 return new Promise((resolve,reject)=>{
   setTimeout(()=>{
+    let num =Math.floor(Math.random()*5)+1;
+    if(num>3){
+      reject("promise rejected");
+    }
     h1.style.color=color;
     console.log(`color changed to ${color}!`);
     resolve("color changed");
@@ -25,9 +29,14 @@ return new Promise((resolve,reject)=>{
 });
 }
 async function demo(params) {
-  await changeColor("red",1000);
+  try{
+await changeColor("red",1000);
    await changeColor("green",1000);
     await changeColor("blue",1000);
-      changeColor("pink",1000);
+     await changeColor("pink",1000);
+  }catch(err){
+    console.log(err);
+  }
+  
   
 }
