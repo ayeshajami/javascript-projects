a2e1580868c4c76e381bc7e343d4fad7a885462e
AyeshadevX <aishaasghar436@gmail.com>
2025-08-28 23:35:46 +0500

    Fix button click event to correctly display fetched fact and enhance error handling in getfacts function

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ let btn = document.querySelector("button");
 btn.addEventListener("click", async () => {
   let fact = await getfacts();   
   console.log(fact);
+let p = document.getElementById("results");
+
+  p.innerText=fact;
 });
 
 let url = "https://catfact.ninja/fact";
+
 
 async function getfacts() {
   try {
     let res = await axios.get(url);
     return res.data.fact;  
+  } catch (e) {
     console.log("Error - ", e);
     return "no fact found";
   }
