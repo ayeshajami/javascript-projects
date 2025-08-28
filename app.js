877331909103c643c8e00e46a988ae1dd357fbdc
AyeshadevX <aishaasghar436@gmail.com>
2025-08-28 23:50:51 +0500

    Add axios config for JSON response in getfacts function

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const url="https://icanhazdadjoke.com/";
 
 async function name() {
   try{
-    let res=await axios.get(url);
+    const config={headers:{Accept:"application/json"}};
+    let res=await axios.get(url,config);
     console.log(res);
   }catch(err){
     console.log(err);
