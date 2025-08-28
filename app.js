566fe824190d6561c088c17c09d8097790b5fc39
AyeshadevX <aishaasghar436@gmail.com>
2025-08-28 23:17:20 +0500

    Refactor app.js to add button click event listener and improve error logging in getfacts function; update index.html structure for better readability

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-let url="https://catfact.ninja/fact";
+let btn=document.querySelector("button");
+btn.addEventListener("click",()=>{
+  console.log("Button clicked");
+});
 
 
+
+
+let url = "https://catfact.ninja/fact";
 
 async function getfacts() {
-  try{
-    let res=await axios.get(url);
+  try {
+    let res = await axios.get(url);
     console.log(res);
-  }catch (e){
-    console.log("Error-"e);
+  } catch (e) {
+    console.log("Error - " + e);
   }
 }
