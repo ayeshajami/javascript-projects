6257e70f8f8797d7812797936f26d8ee4bad664d
AyeshadevX <aishaasghar436@gmail.com>
2025-08-28 22:47:47 +0500

    Refactor demo function to replace changeColor calls with getfacts implementation and enhance error handling

--- app.js
+++ app.js
@@ -1,42 +1,8 @@
-// function getNum(){
-//   return new Promise((resolve,reject)=>{
-//    setTimeout(()=>{
-//      let num=Math.floor(Math.random()*10)+1;
-//     console.log(num);
-//     resolve();
-//    },1000);
-//    });
-// }
-// async function demo() {
-// await  getNum();
-// await  getNum();
-//   getNum();
-// }
-
-h1=document.querySelector("h1");
-
-function changeColor(color,delay){
-return new Promise((resolve,reject)=>{
-  setTimeout(()=>{
-    let num =Math.floor(Math.random()*5)+1;
-    if(num>3){
-      reject("promise rejected");
-    }
-    h1.style.color=color;
-    console.log(`color changed to ${color}!`);
-    resolve("color changed");
-  },delay)
-});
-}
-async function demo(params) {
+async function getfacts() {
   try{
-await changeColor("red",1000);
-   await changeColor("green",1000);
-    await changeColor("blue",1000);
-     await changeColor("pink",1000);
-  }catch(err){
-    console.log(err);
+    let res=await axios.get(url);
+    console.log(res);
+  }catch (e){
+    console.log("Error-"e);
   }
-  
-  
 }
