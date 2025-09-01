9cef00974d5d248eba0dce135b5edc8a0054e305
AyeshadevX <aishaasghar436@gmail.com>
2025-09-01 22:23:52 +0500

    Refactor app.js to use ES6 import syntax for math module

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-// app.js
-const math = require("./math.js");
-
-console.log(math);          // { sum: [Function], mul: [Function], g: 9.8, pi: 3.14 }
-console.log(math.sum(2,3)); // 5
-console.log(math.mul(4,5)); // 20
-console.log(math.g);        // 9.8
-console.log(math.pi);       // 3.14
+import {sum,pi} from './math.js';
