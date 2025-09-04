661d5f96f5e8a8a139f5a75b943a8bcdee3d46f7
AyeshadevX <aishaasghar436@gmail.com>
2025-09-04 20:24:13 +0500

    pro

--- index.js
+++ index.js
@@ -2,9 +2,45 @@ const express = require("express");
 const app = express();
 const port =8080;
 const path=require("path");
+
+app.use(express.urlencoded({extended:true}));
+
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
-app.set(express.static(path.join(__dirname,"public")));
+
+
+app.use(express.static(path.join(__dirname,"public")));
+
+let posts=[
+    {
+        username:"Aisha",
+        content:"Hello Everyone!",
+    },
+     {
+        username:"Sultan",
+        content:"My name is Sultan!",
+    },
+     {
+        username:"Zainab",
+        content:"Hello I am zainab!",
+    },
+
+];
+
+app.get("/posts", (req, res) => {
+    res.render("index.ejs",{posts});
+});
+
+app.get("/posts/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) => {
+    let {username,content}=req.body;
+    posts.push({username,content});
+    res.redirect("/posts");
+});
+
 app.listen(port,()=>{
     console.log("listening to port :8080");
 });
