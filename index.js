6adef574b155a34b940962b8404da13cef794a12
AyeshadevX <aishaasghar436@gmail.com>
2025-09-04 21:15:03 +0500

    pro

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port =8080;
 const path=require("path");
+const { v4: uuidv4 } = require('uuid');
+
 
 app.use(express.urlencoded({extended:true}));
 
@@ -13,17 +15,17 @@ app.use(express.static(path.join(__dirname,"public")));
 
 let posts=[
     {
-        id:"1a",
+        id:uuidv4(),
         username:"Aisha",
         content:"Hello Everyone!",
     },
      {
-        id:"2b",
+        id:uuidv4(),
         username:"Sultan",
         content:"My name is Sultan!",
     },
      {
-        id:"3c",
+        id:uuidv4(),
         username:"Zainab",
         content:"Hello I am zainab!",
     },
@@ -40,16 +42,22 @@ app.get("/posts/new", (req, res) => {
 
 app.post("/posts", (req, res) => {
     let {username,content}=req.body;
-    posts.push({username,content});
+    let id=uuidv4();
+    posts.push({id,username,content});
     res.redirect("/posts");
 });
-
 app.get("/posts/:id", (req, res) => {
-   let {id}=req.params;
+   let {id} = req.params;
    console.log(id);
    let post = posts.find((p) => id === p.id);
-   res.render("show.ejs",{post});
+
+   if (!post) {
+       return res.status(404).send("Post not found");
+   }
+
+   res.render("show.ejs", { post });
 });
+
 
 
 app.listen(port,()=>{
